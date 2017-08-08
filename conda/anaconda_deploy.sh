f18857d8871f7c0e13bf91142138f84bc5eeb0ef
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2017-08-08 10:31:32 +0200

    Update conda build for deploy

--- conda/anaconda_deploy.sh
+++ conda/anaconda_deploy.sh
@@ -5,6 +5,7 @@ export TKN=$2
 export GIT_BRANCH=$3
 
 conda install conda-build anaconda-client --yes
+conda update -n root conda-build anaconda-client --yes
 conda build conda --no-anaconda-upload
 TRG=`conda build conda --output |sed -e 's/--/-*-/'`
 echo "Uploading: $TRG"
