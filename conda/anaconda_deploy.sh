86f499417db484d9a14d1f456e81958f1fc1d660
P.T. Jochym <pawel.jochym@ifj.edu.pl>
2017-08-08 12:05:26 +0200

    Clean up anconda deploy

--- conda/anaconda_deploy.sh
+++ conda/anaconda_deploy.sh
@@ -4,8 +4,7 @@ export LBL=$1
 export TKN=$2
 export GIT_BRANCH=$3
 
-conda install conda-build anaconda-client --yes
-conda update -n root conda-build anaconda-client --yes
+echo "Building: $GIT_BRANCH"
 conda build conda --no-anaconda-upload
 TRG=`conda build conda --output |sed -e 's/--/-*-/'`
 echo "Uploading: $TRG"
